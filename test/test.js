5e4b827d09b54465d0c44226aebc18ac05e88deb
Michael Wittig <michael@widdix.de>
2019-03-26 13:21:53 +0100

    Added secret support (based on #4)

--- test/test.js
+++ test/test.js
@@ -11,3 +11,14 @@ test('defaults', async t => {
     t.pass();
   }
 });
+
+test('with-secret', async t => {
+  const stackName = cfntest.stackName();
+  try {
+    t.log(await cfntest.createStack(`${__dirname}/with-secret.yml`, stackName, {}));
+    // what could we test here?
+  } finally {
+    t.log(await cfntest.deleteStack(stackName));
+    t.pass();
+  }
+});
