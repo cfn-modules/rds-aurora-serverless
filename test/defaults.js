4e4c44e25668a32174f4b83c114b6b447f7a95af
Michael Wittig <michael@widdix.de>
2020-10-05 19:27:42 +0200

    support MySQL 5.7 (#19)

--- test/defaults.js
+++ test/defaults.js
@@ -11,14 +11,3 @@ test.serial('defaults', async t => {
     t.pass();
   }
 });
-
-test.serial('with-secret', async t => {
-  const stackName = cfntest.stackName();
-  try {
-    t.log(await cfntest.createStack(`${__dirname}/with-secret.yml`, stackName, {}));
-    // what could we test here?
-  } finally {
-    t.log(await cfntest.deleteStack(stackName));
-    t.pass();
-  }
-});
